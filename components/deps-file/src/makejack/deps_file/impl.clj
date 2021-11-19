a92d409233c46250f7c5a6b3e0a0aa6dd2211de6
Hugo Duncan <hugo@hugoduncan.org>
2021-11-18 21:58:21 -0500

    fix: remove debug output (#25)

--- components/deps-file/src/makejack/deps_file/impl.clj
+++ components/deps-file/src/makejack/deps_file/impl.clj
@@ -4,57 +4,6 @@
    [rewrite-clj.node :as n]
    [rewrite-clj.zip :as z]))
 
-
-
-;; (defn update-dep-value
-;;   [artifact-name update-vals src]
-;;   (-> (z/of-string src)
-;;       spy
-;;       (z/find-depth-first
-;;        #(do (prn :a (str artifact-name) (n/string (z/node %)))
-;;             (spy %)(= (str artifact-name) (n/string (z/node %)))))
-;;       spy
-;;       z/right
-;;       z/down
-;;       spy
-;;       (z/find z/right
-;;               #(do
-;;                  (prn :x)(spy %)
-;;                  (= (str ":git/sha") (n/string (z/node %)))))
-;;       z/right
-;;       (z/replace (n/token-node (:git/sha update-vals)))
-;;       z/root-string))
-
-
-;; (defn update-dep-tags
-;;   [z update-vals]
-;;   (-> (z/of-string src)
-;;       spy
-;;       (z/find-depth-first
-;;        #(do (prn :a (str artifact-name) (n/string (z/node %)))
-;;             (spy %)(= (str artifact-name) (n/string (z/node %)))))
-;;       spy
-;;       z/right
-;;       z/down
-;;       spy
-;;       (z/find z/right
-;;               #(do
-;;                  (prn :x)(spy %)
-;;                  (= (str ":git/sha") (n/string (z/node %)))))
-;;       z/right
-;;       (z/replace (n/token-node (:git/sha update-vals)))
-;;       z/root-string))
-
-
-(defn spy
-  ([x] (spy x :spy))
-  ([x label]
-   (if x
-     (when-let [node (z/node x)]
-       (prn label :node (n/tag node) (n/string node)))
-     (prn label :no-location))
-   x))
-
 (defn kw=?
   [kw]
   (fn [z]
@@ -71,14 +20,12 @@
 
 (defn- update-kv
   [k v z]
-  (prn :update-kv (n/sexpr z) k v)
   (if-let [zz (z/get z k)]
     (z/replace zz (n/token-node v))
     z))
 
 (defn- update-in-artifact
   [z update-vals]
-  (prn :update-in-artifact (n/sexpr z))
   (loop [z           z
          update-vals update-vals]
     (let [[[k v] & more] update-vals]
@@ -88,19 +35,14 @@
 
 (defn- update-in-deps-val
   [z artifact-name update-vals]
-  (prn :update-in-deps-val (n/sexpr z))
-  (if-let [zz (z/get z artifact-name;; #((string=? artifact-name)
-                     ;;   (spy % :look-for-artifact))
-                     )]
+  (if-let [zz (z/get z artifact-name)]
     (z/subedit-node zz #(update-in-artifact % update-vals))
     z))
 
 (defn update-in-deps-keys
   [src artifact-name update-vals]
   (loop [z (z/of-string src)]
-    (if-let [zz (z/find-depth-first
-                 z
-                 #((kw=? :deps) (spy % :look-for-dep-key)))]
+    (if-let [zz (z/find-depth-first z (kw=? :deps))]
       (recur
        (->
         (z/right zz)
@@ -123,7 +65,6 @@
   (assert (symbol? artifact-name) ":artifact-name must have a symbol as value")
   (let [f           (-> params deps-edn-path fs/file)
         src         (slurp f)
-        _           (prn :xx f)
         update-vals (select-keys params [:git/sha :git/tag :mvn/version])
         new-s       (update-in-deps-keys src artifact-name update-vals)]
     (if new-s
